8a079151b2d545027482d4532cc92cd343756e5f
s1dhart <sid@olmgroup.com>
2016-07-14 16:33:56 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@ java \
 -DTRACE=true \
 -DDEBUG=true \
 -Dinput.target.db.server.hostname=localhost \
--Dinput.target.db.server.dbname_schema=INST_dev \
--Dinput.target.db.server.port=5432 \
--Dinput.target.install.environment=auto \
+-Dinput.target.db.server.dbname_schema=sidenv1 \
+-Dinput.target.db.server.port=1999 \
+-Dinput.target.install.environment=sidenv1 \
 -Dinput.target.db.username=postgres \
 -Dinput.target.db.password=postgres \
 -Dinput.target.db.platform=POSTGRESQL \
 -Dinput.target.install.user=autouser \
 -Dinput.target.install.group=autogroup \
--jar /media/sf_ecl-installer/target/eclipse-installer-1.3.0-SNAPSHOT.jar \
+-jar /home/sid/eclipse-installer-1.3.0-SNAPSHOT.jar \
 -console \
 -options-system
