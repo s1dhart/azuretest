5c8ffb176a99e4e2fb452bd95706b8022923cd8f
s1dhart <sid@olmgroup.com>
2016-07-15 12:55:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 cd /home/sid
-touch abc
+echo $1, $2, $3 > param.file
 sudo java \
 -DTRACE=true \
 -DDEBUG=true \
 -Dinput.target.db.server.hostname=localhost \
--Dinput.target.db.server.dbname_schema=sidenv1 \
+-Dinput.target.db.server.dbname_schema=$1 \
 -Dinput.target.db.server.port=1999 \
--Dinput.target.install.environment=sidenv1 \
+-Dinput.target.install.environment=$1 \
 -Dinput.target.db.username=postgres \
 -Dinput.target.db.password=postgres \
 -Dinput.target.db.platform=POSTGRESQL \
 -Dinput.target.install.user=autouser \
 -Dinput.target.install.group=autogroup \
--Dinput.target.tomcat.http.port=8080
--Dinput.target.ajp.port=8009
+-Dinput.target.tomcat.http.port=$2
+-Dinput.target.ajp.port=$3
 -jar /home/sid/eclipse-installer-1.3.0-SNAPSHOT.jar \
 -console \
 -options-system
