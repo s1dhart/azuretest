14bffac8de516878c8bedb6c37df2e309ce53bf3
s1dhart <sid@olmgroup.com>
2016-07-18 18:06:31 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 cd /home/sid
 http=$(ss -tln |   awk 'NR > 1{gsub(/.*:/,"",$4); print $4}' |  sort -un |  awk -v n=8080 '$0 < n {next}; $0 == n {n++; next}; {exit}; END {print n}')
 ajp=$(ss -tln |   awk 'NR > 1{gsub(/.*:/,"",$4); print $4}' |  sort -un |  awk -v n=8009 '$0 < n {next}; $0 == n {n++; next}; {exit}; END {print n}')
+ssl=$(ss -tln |   awk 'NR > 1{gsub(/.*:/,"",$4); print $4}' |  sort -un |  awk -v n=443 '$0 < n {next}; $0 == n {n++; next}; {exit}; END {print n}')
 sudo echo $1, $http, $ajp > param.file
 sudo su - postgres -c "createdb -h localhost -p 1999 -U postgres $1"
 sudo java \
@@ -16,8 +17,15 @@ sudo java \
 -Dinput.target.db.platform=POSTGRESQL \
 -Dinput.target.install.user=autouser \
 -Dinput.target.install.group=autogroup \
--Dinput.target.tomcat.http.port=$2 \
--Dinput.target.ajp.port=$3 \
+-Dinput.target.tomcat.http.port=$http \
+-Dinput.target.ajp.port=$ajp \
 -jar /home/sid/eclipse-installer-1.3.0-SNAPSHOT.jar \
 -console \
 -options-system
+sudo cp /etc/nginx/sites-available/eclipse_template /etc/nginx/sites-available/$1
+sudo cd /etc/nginx/sites-available
+sudo sed -i /443/$ssl/g $1
+sudo sed -i s/8011/$ajp/g $1
+sudo sed -i s/sidenv1/$1/g $1
+sudo ln -s /etc/nginx/sites-available/$1 $1
+sudo service nginx reload
